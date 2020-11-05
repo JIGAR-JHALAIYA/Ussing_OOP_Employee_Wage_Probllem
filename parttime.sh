647f0c921130ce87042a3b3637d1145bfb022339
JIGAR-JHALAIYA <jdjhalaiya27@gmail.com>
2020-11-05 11:19:10 +0530

    Part_Time_Emp_Wage_UC3

---
+++ parttime.sh
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+     public static void main(String []args)
+     {
+        System.out.println("Welcome to Employee Wage computation");
+        int wagesperHour=20;
+        int halfdayHours=8;
+        int halfdayWages=wagesPerHour * halfdayHours*
+        System.out.println(halfdayWages);
+     }
+}
