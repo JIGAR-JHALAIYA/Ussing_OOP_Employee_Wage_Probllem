d5a463e996c29fe1a9b47fca86c9896788edf5fb
JIGAR-JHALAIYA <jdjhalaiya27@gmail.com>
2020-11-05 11:11:46 +0530

    Total_Daily_Wage

---
+++ dailywage.sh
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+     public static void main(String []args)
+     {
+        System.out.println("Welcome to Employee Wage computation");
+        int wagesPerHour=20;
+        int FullDayHours=8;
+        int DailyWages=wagesPerHour * FullDayHours*
+        System.out.println(DailWages);
+     }
+}
